cb9e890aec2a572ff611d2ca0381aeaf1ee0b9e3
Frederik Lohse <frederik@lohse.de>
2023-07-05 20:36:48 +0200

    Created classes and some variables and constructor

---
+++ JavaProject/MakeBaguetteProject/src/main/java/BaguetteMaking.java
@@ -0,0 +1,21 @@
+public class BaguetteMaking {
+
+    BaguetteMaking (String brot, int groesseInMeter, int anzahl) {
+
+
+this.brot = brot;
+this.groesseInMeter = groesseInMeter;
+this.anzahl = anzahl;
+
+
+    }
+
+    public String brot;
+
+    public int groesseInMeter;
+
+    public int anzahl;
+
+
+
+}
