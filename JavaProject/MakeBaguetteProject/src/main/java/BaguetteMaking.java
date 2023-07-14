9a414fd22cb56c16d869adfd71cfc4c7801242a1
Frederik Lohse <walrussi@CrazyMacBook.fritz.box>
2023-07-14 10:46:28 +0200

    Added empty methods into the class

--- JavaProject/MakeBaguetteProject/src/main/java/BaguetteMaking.java
+++ JavaProject/MakeBaguetteProject/src/main/java/BaguetteMaking.java
@@ -2,7 +2,9 @@ public class BaguetteMaking {
 
 
     /**
-     * user should be able to create baguettes with salad, tomatoes, cheese like Baguette.build().withBred("weissbrot").withSize(2).withSalad().withCheese().build()
+     * user should be able to create baguettes with
+     * salad, tomatoes, cheese like
+     * Baguette.build().withBred("weissbrot").withSize(2).withSalad().withCheese().build()
      * user should be able to print out an ascii image of a baguette
      */
 
@@ -19,5 +21,29 @@ public class BaguetteMaking {
 
     public int groesseInMeter;
 
+    public void build() {
 
+
+    }
+
+    public void withBread() {
+
+
+    }
+
+    public void withSize
+
+    {
+
+
+    }
+
+    public void withSalad() {
+
+    }
+
+    public void withCheese() {
+
+
+    }
 }
